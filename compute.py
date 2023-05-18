e7ea0a5262750a77d234a2c1e94b3bea971ff34b
VinayakMandlik <133949502+VinayakMandlik@users.noreply.github.com>
2023-05-18 19:23:27 +0530

    demo

---
+++ compute.py
@@ -0,0 +1,16 @@
+num1=int(input(enter num1))
+num2=int(input(enter num2))
+
+num3=int(input(enter num3))
+
+if num1>num2 and num1>num3:
+print(str(num1) is greater)
+
+elif num2>num3 and num2>num3:
+print(str(num2) is greater)
+
+else:
+print(str(num3) is greater)
+
+
+
---
+++ divisor.py
@@ -0,0 +1,8 @@
+num=int(input('ENTER NUM'))
+my_list=[]
+for i in range(1,num+1)
+if(num%i==0):
+my_list.append(i)
+else:
+pass
+print(my_list)
